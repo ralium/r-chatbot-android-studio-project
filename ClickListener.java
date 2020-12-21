c04c9d9963d6bdca7ef2d8ee3587ce3d7cbdfe2d
ralium <76412629+ralium@users.noreply.github.com>
2020-12-21 04:26:43 +0400

    Create ClickListener.java

---
+++ ClickListener.java
@@ -0,0 +1,9 @@
+package com.example.collegeinfobotjava;
+
+import android.view.View;
+
+public interface ClickListener {
+    void onClick(View view, int position);
+
+    void onLongClick(View view, int position);
+}
