3cce95b4f949e7d4f21c74558eec6251c2cdefce
ralium <76412629+ralium@users.noreply.github.com>
2020-12-24 11:21:58 +0400

    Delete ClickListener.java

--- ClickListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.collegeinfobotjava;
-
-import android.view.View;
-
-public interface ClickListener {
-    void onClick(View view, int position);
-
-    void onLongClick(View view, int position);
-}
